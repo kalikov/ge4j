6a6a226dcc6ff4e632b5f834d0bc915b72a68aac
Aleksandr <kalikovar@gmail.com>
2018-09-21 00:11:02 +0500

    Window params

--- ge4j-awt/src/main/java/org/ge4j/awt/AwtGraphicsSystem.java
+++ ge4j-awt/src/main/java/org/ge4j/awt/AwtGraphicsSystem.java
@@ -4,52 +4,19 @@ import org.ge4j.Engine;
 import org.ge4j.GraphicsSystem;
 import org.ge4j.Managed;
 
-import java.awt.DisplayMode;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-
 public class AwtGraphicsSystem implements GraphicsSystem {
-    private static final Logger logger = LoggerFactory.getLogger(AwtGraphicsSystem.class);
-
     private final Engine engine;
     private AwtWindow window;
     private String title = "Game Engine";
     private int width = 640;
     private int height = 480;
-    private boolean isFullscreen = true;
+    private boolean isFullscreen = false;
 
     @Managed
     public AwtGraphicsSystem(Engine engine) {
         this.engine = engine;
         engine.engineStarted().bind(this::onEngineStarted);
         engine.engineStopped().bind(this::onEngineStopped);
-
-        GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
-        GraphicsDevice fullscreenDevice = null;
-        for (GraphicsDevice device : devices) {
-            if (!device.isFullScreenSupported()) {
-                logger.debug("Found device {}");
-                continue;
-            }
-            if (fullscreenDevice == null) {
-                fullscreenDevice = device;
-            } else {
-                DisplayMode deviceMode = device.getDisplayMode();
-                int width = deviceMode.getWidth();
-                int height = deviceMode.getHeight();
-                int bitDepth = deviceMode.getBitDepth();
-                DisplayMode fullscreenMode = fullscreenDevice.getDisplayMode();
-                int fullscreenWidth = fullscreenMode.getWidth();
-                int fullscreenHeight = fullscreenMode.getHeight();
-                int fullscreenBitDepth = fullscreenMode.getBitDepth();
-                if (width < fullscreenWidth || height < fullscreenHeight) {
-                    continue;
-                }
-                if (width > fullscreenWidth || height > fullscreenHeight || bitDepth > fullscreenBitDepth) {
-                    fullscreenDevice = device;
-                }
-            }
-        }
     }
 
     public void onDestroy() {
@@ -124,7 +91,7 @@ public class AwtGraphicsSystem implements GraphicsSystem {
 
     private void adjustWindow() {
         if (window != null) {
-            window.setDimensions(width, height);
+            window.setWindowed(width, height);
         }
     }
 }
--- ge4j-awt/src/main/java/org/ge4j/awt/AwtWindow.java
+++ ge4j-awt/src/main/java/org/ge4j/awt/AwtWindow.java
@@ -1,45 +1,39 @@
 package org.ge4j.awt;
 
-import javax.swing.JFrame;
-import java.awt.Dimension;
-import java.awt.DisplayMode;
-import java.awt.Graphics;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Insets;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class AwtWindow {
-    private final GraphicsDevice fullscreenDevice;
     private final JFrame frame;
+//    private final BufferStrategy bufferStrategy;
 
     private Runnable closeHandler;
 
+    private int windowedWidth;
+    private int windowedHeight;
+
     AwtWindow(String title, int width, int height, boolean isFullscreen) {
-
-
         frame = new JFrame(title) {
-            @Override
-            public void paint(Graphics gfx) {
-            }
+//            @Override
+//            public void paint(Graphics gfx) {
+//            }
         };
-//        frame.setBackground(SCREEN_BG_COLOR);
-//        frame.setSize(Screen.ORIGINAL_DISPLAY_WIDTH, Screen.ORIGINAL_DISPLAY_HEIGHT);
-        frame.setIgnoreRepaint(true);
+        GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+        DisplayMode displayMode = device.getDisplayMode();
+
+        windowedWidth = Math.min(width, displayMode.getWidth());
+        windowedHeight = Math.min(height, displayMode.getHeight());
+
+        int x = (displayMode.getWidth() - windowedWidth) / 2;
+        int y = (displayMode.getHeight() - windowedHeight) / 2;
+        frame.setBounds(x, y, windowedWidth, windowedHeight);
+
+//        frame.setIgnoreRepaint(true);
+        frame.setResizable(false);
         frame.setVisible(true);
-
-        Insets insets = frame.getInsets();
-        int frameWidth = width + insets.right + insets.left;
-        int frameHeight = height + insets.bottom + insets.top;
-        frame.setSize(frameWidth, frameHeight);
-
-        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation((screen.width - frameWidth) / 2, (screen.height - frameHeight) / 2);
-//
-//        frame.createBufferStrategy(2);
-//        bufferStrategy = frame.getBufferStrategy();
 
         frame.addWindowListener(new WindowAdapter() {
             @Override
@@ -49,6 +43,59 @@ class AwtWindow {
                 }
             }
         });
+
+//        frame.createBufferStrategy(2);
+//        bufferStrategy = frame.getBufferStrategy();
+
+        JButton button1 = new JButton("FS");
+        button1.addActionListener(e -> {
+            toggleFullscreen();
+        });
+
+        JButton button2 = new JButton("800");
+        button2.addActionListener(e -> {
+            setWindowed(800, 600);
+        });
+
+        JButton button3 = new JButton("640");
+        button3.addActionListener(e -> {
+            setWindowed(640, 480);
+        });
+
+        AtomicInteger i = new AtomicInteger();
+        JButton button4 = new JButton("X");
+        button4.addActionListener(e -> {
+            int v =i.incrementAndGet() % 4;
+            if (v == 1) {
+                frame.setLocation(device.getDefaultConfiguration().getBounds().x, 0);
+            } else if (v == 2) {
+                frame.setLocation(displayMode.getWidth() - frame.getWidth(), 0);
+            } else if (v == 3) {
+                frame.setLocation(displayMode.getWidth() - frame.getWidth(), displayMode.getHeight() - frame.getHeight());
+            } else {
+                frame.setLocation(0, displayMode.getHeight() - frame.getHeight());
+            }
+        });
+
+        JPanel panel = new JPanel();
+        panel.add(button1);
+        panel.add(button2);
+        panel.add(button3);
+        panel.add(button4);
+        frame.getContentPane().add(panel);
+
+        if (isFullscreen) {
+            device.setFullScreenWindow(frame);
+        } else {
+            Insets insets = frame.getInsets();
+            windowedWidth = Math.min(windowedWidth, displayMode.getWidth() - insets.right - insets.left);
+            windowedHeight = Math.min(windowedHeight, displayMode.getHeight() - insets.bottom - insets.top);
+
+            int frameWidth = windowedWidth + insets.right + insets.left;
+            int frameHeight = windowedHeight + insets.bottom + insets.top;
+
+            frame.setBounds((displayMode.getWidth() - frameWidth) / 2, (displayMode.getHeight() - frameHeight) / 2, frameWidth, frameHeight);
+        }
     }
 
     void setCloseHandler(Runnable closeHandler) {
@@ -63,11 +110,87 @@ class AwtWindow {
         frame.setTitle(title);
     }
 
-    public void setDimensions(int width, int height) {
-        frame.setSize(width, height);
+    public void setWindowed(int width, int height) {
+        GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+        if (device.getFullScreenWindow() == frame) {
+            device.setFullScreenWindow(null);
+        }
+        DisplayMode displayMode = device.getDisplayMode();
+
+        int centerX = displayMode.getWidth() / 2;
+        int centerY = displayMode.getHeight() / 2;
+
+        Insets insets = frame.getInsets();
+        windowedWidth = Math.min(width, displayMode.getWidth() - insets.right - insets.left);
+        windowedHeight = Math.min(height, displayMode.getHeight() - insets.bottom - insets.top);
+
+        int frameWidth = windowedWidth + insets.right + insets.left;
+        int frameHeight = windowedHeight + insets.bottom + insets.top;
+
+        int minX = frameWidth / 2;
+        int maxX = displayMode.getWidth() - frameWidth / 2;
+        int minY = frameHeight / 2;
+        int maxY = displayMode.getHeight() - frameHeight / 2;
+
+        Point location = frame.getLocationOnScreen();
+        location.translate(frame.getWidth() / 2, frame.getHeight() / 2);
+
+        int newX;
+        if (frameWidth > frame.getWidth()) {
+            newX = Math.min(maxX, Math.max(minX, frameWidth * (location.x - centerX) / frame.getWidth() + centerX));
+        } else {
+            newX = Math.min(maxX, Math.max(minX, frame.getWidth() * (location.x - centerX) / frameWidth + centerX));
+        }
+        int newY;
+        if (frameHeight > frame.getHeight()) {
+            newY = Math.min(maxY, Math.max(minY, frameHeight * (location.y - centerY) / frame.getHeight() + centerY));
+        } else {
+            newY = Math.min(maxY, Math.max(minY, frame.getHeight() * (location.y - centerY) / frameHeight + centerY));
+        }
+
+        frame.setBounds(newX - frameWidth / 2, newY - frameHeight / 2, frameWidth, frameHeight);
     }
 
     public void toggleFullscreen() {
+        GraphicsDevice device = frame.getGraphicsConfiguration().getDevice();
+        if (device.getFullScreenWindow() == frame) {
+            device.setFullScreenWindow(null);
+        } else {
+            device.setFullScreenWindow(frame);
+        }
+    }
 
+    public void flip() {
+//        Insets insets = frame.getInsets();
+//        do {
+//            do {
+//                Graphics2D gfx = (Graphics2D) bufferStrategy.getDrawGraphics();
+//                gfx.translate(insets.left, insets.top);
+//                gfx.scale(scaleX, scaleY);
+//                try {
+//                    gfx.drawImage(surface.getImage(), 0, 0, SCREEN_BG_COLOR, null);
+//                    if (cursor != null) {
+//                        Point screenOffset = null;
+//                        synchronized (frame.getTreeLock()) {
+//                            if (frame.isShowing()) {
+//                                screenOffset = frame.getLocationOnScreen();
+//                            }
+//                        }
+//                        if (screenOffset != null) {
+//                            Surface cursorSurface = cursor.getSurface();
+//                            if (cursorSurface instanceof AwtSurface) {
+//                                Point mousePoint = MouseInfo.getPointerInfo().getLocation();
+//                                int x = (int) ((mousePoint.getX() - screenOffset.getX() - insets.left) / scaleX);
+//                                int y = (int) ((mousePoint.getY() - screenOffset.getY() - insets.top) / scaleY);
+//                                gfx.drawImage(((AwtSurface) cursorSurface).getImage(), x, y, null);
+//                            }
+//                        }
+//                    }
+//                } finally {
+//                    gfx.dispose();
+//                }
+//            } while (bufferStrategy.contentsRestored());
+//            bufferStrategy.show();
+//        } while (bufferStrategy.contentsLost());
     }
 }
